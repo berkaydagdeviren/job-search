b5fbac547cf37d8654b99c48f22275df564fd9b1
berkaydagdeviren <berkaydagdeviren2626@gmail.com>
2022-07-09 16:07:09 +0300

    added primary&secondary button

--- src/components/MainNav.vue
+++ src/components/MainNav.vue
@@ -2,19 +2,17 @@
   <header class="w-full text-sm">
     <div class="fixed top-0 left-0 w-full h-16 bg-white">
       <div
-        class="flex flex-nowrap h-full px-8 mx-auto border-b border-solid border-brand-gray-1"
+        class="flex flex-nowrap h-full px-8 border-b border-solid border-brand-gray-1"
       >
         <a href="/" class="flex items-center text-xl"> {{ company }} </a>
 
         <nav class="ml-12 h-full">
           <ul class="flex p-0 m-0 list-none h-full">
-            <li class="h-full">
-              <a href="" class="flex items-center h-full py-2.5">Teams</a>
-            </li>
             <li
               v-for="menuItem in menuItems"
               :key="menuItem"
-              class="h-full ml-9"
+              class="h-full first:ml-0 ml-9"
+              data-test="main-nav-list-item"
             >
               <a href="" class="flex items-center h-full py-2.5">{{
                 menuItem
@@ -22,13 +20,27 @@
             </li>
           </ul>
         </nav>
+        <div class="flex items-center h-full ml-auto">
+          <profile-image v-if="isLoggedIn" data-test="profile-image" />
+          <action-button
+            v-else
+            text="Sign in"
+            type="primary"
+            data-test="login-button"
+            @click="loginUser"
+          />
+        </div>
       </div>
     </div>
   </header>
 </template>
 <script>
+import ActionButton from "./ActionButton.vue";
+import ProfileImage from "./ProfileImage.vue";
+
 export default {
   name: "MainNav",
+  components: { ActionButton, ProfileImage },
   data() {
     return {
       company: "Radical Careers",
@@ -40,7 +52,13 @@ export default {
         "Students",
         "Jobs",
       ],
+      isLoggedIn: false,
     };
+  },
+  methods: {
+    loginUser() {
+      this.isLoggedIn = true;
+    },
   },
 };
 </script>
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,12 +8,21 @@ module.exports = {
       fontFamily: {
         sans: ["Open Sans", ...defaultTheme.fontFamily.sans],
       },
-    },
-    colors: {
-      "brand-gray-1": "#dadce0",
-      "brand-blue-1": "#1967d2",
-      "brand-green-1": "#137333",
+      colors: {
+        "brand-gray-1": "#dadce0",
+        "brand-blue-1": "#1967d2",
+        "brand-green-1": "#137333",
+        "brand-blue-2": "#4285f4",
+      },
+      boxShadow: {
+        blue: "0 0 3px 3px #4285f4",
+      },
     },
   },
   plugins: [],
+  variants: {
+    extend: {
+      margin: ["first"],
+    },
+  },
 };
